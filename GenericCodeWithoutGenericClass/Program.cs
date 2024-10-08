ff6b7887bfbd1cac783978aeb95acbce35f64a45
alif.meydi <alif.meydi@rds.co.id>
2024-10-08 13:56:43 +0700

    feat: first commit

---
+++ GenericCodeWithoutGenericClass/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace GenericCodeWithoutGenericClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = new Logger();
+            logger.Log<int>(1);
+            logger.Log<string>("string nih");
+            logger.Log<bool>(false);
+            logger.Log<object>(new {Name = "Alif", Age = 21});
+        }
+    }
+}
